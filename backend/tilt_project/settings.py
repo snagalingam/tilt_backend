191bc275a945276bee5c18fb980b2489bfa0e5d6
Sinthuja Nagalingam <sinthu.n@gmail.com>
2020-03-30 16:12:43 -0500

    environment variables

--- backend/tilt_project/settings.py
+++ backend/tilt_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '((5qzn4=1s57*63r48lqkvz%60d#&_@b4w5f05$6)lu35r(^lm'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = ['*']
 
