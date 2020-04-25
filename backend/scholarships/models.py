d76ac41c223e9665b002e8326fd5044494e7f937
Sinthuja Nagalingam <sinthu.n@gmail.com>
2020-04-25 10:56:42 -0500

    updated scholarship model

--- backend/scholarships/models.py
+++ backend/scholarships/models.py
@@ -1,12 +1,33 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
 class Scholarship(models.Model):
-    name = models.CharField(max_length=250)
-    amount = models.IntegerField()
-    deadline = models.DateField()
+
+    # user inputted
+    name = models.CharField(max_length=250, unique_for_date='deadline')
+    organization = models.CharField(max_length=250, blank=True, null=True)
     url = models.URLField(max_length=1000)
+    due_date = models.DateField()
+    max_amount = models.IntegerField(null=True, blank=True)
+    renewable = models.BooleanField(null=True, blank=True)
+    category = models.CharField(max_length=250, null=True, blank=True)
+    area_of_study = models.CharField(max_length=250, null=True, blank=True)
+    location = models.CharField(max_length=250, null=True, blank=True)
+    state = models.CharField(max_length=2, null=True, blank=True)
+    ethnicity = models.CharField(max_length=250, null=True, blank=True)
+    gender = models.CharField(max_length=250, null=True, blank=True)
+    gpa = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    us_citizen = models.BooleanField()
+    daca_status = models.BooleanField()
+    financial_need = models.BooleanField()
+    hbcu = models.BooleanField()
+
+    # automatically added
+    author = models.ForeignKey(get_user_model(), null=True, on_delete=models.SET_NULL)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
--- backend/scholarships/tests.py
+++ backend/scholarships/tests.py
@@ -9,19 +9,101 @@ class ScholarshipTests(TestCase):
     def setUpTestData(cls):
         Scholarship.objects.create(
             name='first scholarship',
+            organization='organization',
             url='https://www.scholarship.com',
-            amount='1000',
-            deadline='2020-03-31',
+            due_date='2020-03-31',
+            max_amount='1000',
+            renewable=True,
+            area_of_study='Business',
+            location='Chicago',
+            state='IL',
+            ethnicity='Asian',
+            gender='Woman',
+            gpa='4.00',
+            us_citizen=True,
+            daca_status=True,
+            financial_need=True,
+            hbcu=True,
         )
 
-    def test_scholarship_content(self):
+        Scholarship.objects.create(
+            name='second scholarship',
+            url='https://www.secondscholarship.com',
+            due_date='2020-04-01',
+            us_citizen=False,
+            daca_status=False,
+            financial_need=False,
+            hbcu=False,
+        )
+
+    def test_scholarship_content_without_blanks(self):
         scholarship = Scholarship.objects.get(id=1)
         name = f'{scholarship.name}'
+        organization = f'{scholarship.organization}'
         url = f'{scholarship.url}'
-        amount = f'{scholarship.amount}'
-        deadline = f'{scholarship.deadline}'
+        due_date = f'{scholarship.due_date}'
+        max_amount = f'{scholarship.max_amount}'
+        renewable = scholarship.renewable
+        area_of_study = f'{scholarship.area_of_study}'
+        location = f'{scholarship.location}'
+        state = f'{scholarship.state}'
+        ethnicity = f'{scholarship.ethnicity}'
+        gender = f'{scholarship.gender}'
+        gpa = f'{scholarship.gpa}'
+        us_citizen = scholarship.us_citizen
+        daca_status = scholarship.daca_status
+        financial_need = scholarship.financial_need
+        hbcu = scholarship.hbcu
+
         self.assertEquals(name, 'first scholarship')
+        self.assertEquals(organization, 'organization')
         self.assertEquals(url, 'https://www.scholarship.com')
-        self.assertEquals(amount, '1000')
-        self.assertEquals(amount_descriptor, 'exact')
-        self.assertEquals(deadline, '2020-03-31')
+        self.assertEquals(due_date, '2020-03-31')
+        self.assertEquals(max_amount, '1000')
+        self.assertTrue(renewable)
+        self.assertEquals(area_of_study, 'Business')
+        self.assertEquals(location, 'Chicago')
+        self.assertEquals(state, 'IL')
+        self.assertEquals(ethnicity, 'Asian')
+        self.assertEquals(gender, 'Woman')
+        self.assertEquals(gpa, '4.00')
+        self.assertTrue(us_citizen)
+        self.assertTrue(daca_status)
+        self.assertTrue(financial_need)
+        self.assertTrue(hbcu)
+
+    def test_scholarship_content_with_blanks(self):
+        scholarship = Scholarship.objects.get(id=2)
+        name = f'{scholarship.name}'
+        organization = scholarship.organization
+        url = f'{scholarship.url}'
+        due_date = f'{scholarship.due_date}'
+        max_amount = scholarship.max_amount
+        renewable = scholarship.renewable
+        area_of_study = scholarship.area_of_study
+        location = scholarship.location
+        state = scholarship.state
+        ethnicity = scholarship.ethnicity
+        gender = scholarship.gender
+        gpa = scholarship.gpa
+        us_citizen = scholarship.us_citizen
+        daca_status = scholarship.daca_status
+        financial_need = scholarship.financial_need
+        hbcu = scholarship.hbcu
+
+        self.assertEquals(name, 'second scholarship')
+        self.assertIsNone(organization)
+        self.assertEquals(url, 'https://www.secondscholarship.com')
+        self.assertEquals(due_date, '2020-04-01')
+        self.assertIsNone(max_amount)
+        self.assertIsNone(renewable)
+        self.assertIsNone(area_of_study)
+        self.assertIsNone(location)
+        self.assertIsNone(state)
+        self.assertIsNone(ethnicity)
+        self.assertIsNone(gender)
+        self.assertIsNone(gpa)
+        self.assertFalse(us_citizen)
+        self.assertFalse(daca_status)
+        self.assertFalse(financial_need)
+        self.assertFalse(hbcu)
