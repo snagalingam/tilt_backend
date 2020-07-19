4c66b91defe6b13b6e74cf9d720c55d909bbd523
Johnny Siu <siujohnny@gmail.com>
2020-07-18 18:54:05 -0700

    update with django-heroku

---
+++ frontend/build/precache-manifest.ef4e972864cc52ebb1ecebfafa98cc89.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70cab425235b0ff8a1286ad8caa30723",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0af3de7d1661dada5424",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "43fe9419b5caa2c9edef",
+    "url": "/static/css/main.aa1fa87e.chunk.css"
+  },
+  {
+    "revision": "0af3de7d1661dada5424",
+    "url": "/static/js/2.e392285c.chunk.js"
+  },
+  {
+    "revision": "f289167843099abbfefae448f28f8aeb",
+    "url": "/static/js/2.e392285c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43fe9419b5caa2c9edef",
+    "url": "/static/js/main.f3575d48.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "65d68bd4f2d5cd44c99ab7b30bda3b01",
+    "url": "/static/media/Feather.65d68bd4.svg"
+  },
+  {
+    "revision": "d9c23068ef426a94453f85a8c1a695b5",
+    "url": "/static/media/Feather.d9c23068.woff"
+  },
+  {
+    "revision": "fe1594343a6aed9427c646993d06ea9c",
+    "url": "/static/media/Feather.fe159434.ttf"
+  },
+  {
+    "revision": "3e00014e1cb8fae7e568759d6112193f",
+    "url": "/static/media/HKGroteskPro-Medium.3e00014e.woff2"
+  },
+  {
+    "revision": "9c9e142d9177ed246a4a74715e46701b",
+    "url": "/static/media/HKGroteskPro-Medium.9c9e142d.woff"
+  },
+  {
+    "revision": "2fed1725e4fba84fd3d0d701f577be18",
+    "url": "/static/media/HKGroteskPro-Regular.2fed1725.woff2"
+  },
+  {
+    "revision": "664e1f143aa0011593524e11eba36b65",
+    "url": "/static/media/HKGroteskPro-Regular.664e1f14.woff"
+  },
+  {
+    "revision": "ca81c69a53bd74386fd673e6d9ea347f",
+    "url": "/static/media/Tilt_Sample.ca81c69a.pdf"
+  },
+  {
+    "revision": "6ac726130dcb2db3245cbd2b42a47685",
+    "url": "/static/media/avatar-group-hover-last.6ac72613.svg"
+  },
+  {
+    "revision": "672e50f73963cf97baae196cf2c7a7bd",
+    "url": "/static/media/avatar-group-hover.672e50f7.svg"
+  },
+  {
+    "revision": "936a2d3aedfe174ffaf564808de152fe",
+    "url": "/static/media/avatar-group.936a2d3a.svg"
+  },
+  {
+    "revision": "7ca4c2012feeb8ac3a01a078c0177b36",
+    "url": "/static/media/avatar-status.7ca4c201.svg"
+  },
+  {
+    "revision": "ba76327da5b060e5db4dbc9f1828a137",
+    "url": "/static/media/butler.ba76327d.svg"
+  },
+  {
+    "revision": "4fa14a84ab0c08f891811ac720b01b80",
+    "url": "/static/media/chart.4fa14a84.jpg"
+  },
+  {
+    "revision": "04fd16d029a82b62446bdf2916a23e0a",
+    "url": "/static/media/cover.04fd16d0.jpg"
+  },
+  {
+    "revision": "75147f8254549defe6aa5639afc47905",
+    "url": "/static/media/facebook.75147f82.svg"
+  },
+  {
+    "revision": "050329b76222f5e2c46d3465cbab6cb3",
+    "url": "/static/media/hfs.050329b7.svg"
+  },
+  {
+    "revision": "d52bf409343a2e74cc7143ebde7656c0",
+    "url": "/static/media/illustration-1.d52bf409.png"
+  },
+  {
+    "revision": "06ebcd23cb359b37869107eebbdf497f",
+    "url": "/static/media/instagram.06ebcd23.svg"
+  },
+  {
+    "revision": "ccae1a6c4be6fc0a67b8d990d6962850",
+    "url": "/static/media/solorio.ccae1a6c.svg"
+  },
+  {
+    "revision": "e727179e5b9203dd5c5867a5ecac5123",
+    "url": "/static/media/tilt_logo.e727179e.png"
+  },
+  {
+    "revision": "938d0de67b3b496a7db1d7a2cd067fb4",
+    "url": "/static/media/twitter.938d0de6.svg"
+  },
+  {
+    "revision": "9d69289e7af67c563d6ce24af6545843",
+    "url": "/static/media/westinghouse.9d69289e.svg"
+  }
+]);
