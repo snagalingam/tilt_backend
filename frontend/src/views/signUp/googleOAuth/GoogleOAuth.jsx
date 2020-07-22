3645a586dc01e7c57c7d62724a85c41b727f69fe
Tony Yu <toekneeyou@tonys-mbp.lan>
2020-07-21 21:53:27 -0700

    styled a signup form, not functional yet

---
+++ frontend/src/views/signUp/googleOAuth/GoogleOAuth.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./google-oauth.scss";
+
+const GoogleOAuth = () => {
+  return (
+    <button className="google-oauth-button">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1004px-Google_%22G%22_Logo.svg.png" />
+      Sign up with Google
+    </button>
+  );
+};
+
+export default GoogleOAuth;
---
+++ frontend/src/views/signUp/signUpForm/SignUpForm.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import * as yup from "yup";
+
+import "./sign-up-form.scss";
+
+const signUpSchema = yup.object().shape({
+  firstName: yup.string().required("Please enter your first name"),
+  lastName: yup.string().required("Please enter your last name"),
+  email: yup
+    .string()
+    .email("Invalid email")
+    .required("Please enter your email"),
+  password1: yup.string().required("Password is required"),
+  password2: yup.string().required("Please confirm your password"),
+});
+
+const FieldSet = ({
+  name,
+  label,
+  fieldType,
+  children,
+  type,
+  errors,
+  touched,
+}) => (
+  <fieldset>
+    <label htmlFor={`signup-${name}`}>{label}</label>
+    <Field as={fieldType} name={name} type={type}>
+      {children}
+    </Field>
+    {errors[name] && touched[name] && (
+      <span className="form-error">{errors[name]}</span>
+    )}
+  </fieldset>
+);
+
+const SignUpForm = () => {
+  function handleSubmit(values) {
+    console.log(values);
+  }
+
+  return (
+    <Formik
+      initialValues={{
+        email: "",
+        firstName: "",
+        lastName: "",
+        password: undefined,
+      }}
+      validationSchema={signUpSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ errors, touched }) => (
+        <Form className="sign-up-form form">
+          <FieldSet
+            name="email"
+            label="Email"
+            errors={errors}
+            touched={touched}
+          />
+          <div className="form-name-fields">
+            <FieldSet
+              name="firstName"
+              label="First Name"
+              errors={errors}
+              touched={touched}
+            />
+            <FieldSet
+              name="lastName"
+              label="Last Name"
+              errors={errors}
+              touched={touched}
+            />
+          </div>
+          <FieldSet
+            name="password1"
+            label="Password"
+            type="password"
+            errors={errors}
+            touched={touched}
+          />
+          <FieldSet
+            name="password2"
+            label="Confirm Password"
+            type="password"
+            errors={errors}
+            touched={touched}
+          />
+
+          <div className="sign-up-terms-of-service">
+            <p>
+              By signing up, you agree to our Terms of Service and Privacy
+              Policy
+            </p>
+          </div>
+
+          <button>Create Account</button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default SignUpForm;
