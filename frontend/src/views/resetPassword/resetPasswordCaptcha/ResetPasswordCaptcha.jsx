751585184495977082989de22a03a976b4cda7d6
Tony Yu <toekneeyou@tonys-mbp.lan>
2020-07-22 18:30:58 -0700

    added reCaptcha

--- frontend/src/views/resetPassword/resetPasswordCaptcha/ResetPasswordCaptcha.jsx
+++ frontend/src/views/resetPassword/resetPasswordCaptcha/ResetPasswordCaptcha.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { ReCaptcha } from "react-recaptcha-google";
+import { Link } from "react-router-dom";
+
+import "./reset-password-recaptcha.scss";
 
 const ResetPasswordCaptcha = ({ setCurrentView, views }) => {
+  const captchaRef = useRef(null);
+
   const { CONFIRM } = views;
-  function handleSuccess() {
-    setCurrentView(CONFIRM);
+
+  function onLoadRecaptcha() {
+    if (captchaRef.current) {
+      captchaRef.current.reset();
+    }
   }
+
+  useEffect(() => {
+    onLoadRecaptcha();
+  }, []);
+
+  function verifyCallback(recaptchaToken) {
+    // API for verifying token with server
+    // https://www.google.com/recaptcha/api/siteverify
+    if (recaptchaToken) {
+      setCurrentView(CONFIRM);
+      // TODO: send reset password email
+    } else {
+      captchaRef.current.reset();
+    }
+  }
+
   return (
     <div className="reset-password-captcha-view">
       <div className="form-container">
@@ -15,9 +40,25 @@ const ResetPasswordCaptcha = ({ setCurrentView, views }) => {
             accessing your Tilt account.
           </p>
         </div>
-        <button onClick={handleSuccess}>Pretend this is captcha</button>
+
+        <div className="recaptcha-container">
+          <ReCaptcha
+            ref={(el) => {
+              captchaRef.current = el;
+            }}
+            size="normal"
+            data-theme="dark"
+            render="explicit"
+            sitekey="6Ley1bQZAAAAAAfbuBKAeVO1NM6fNhRUMm1eCPDH"
+            onloadCallback={onLoadRecaptcha}
+            verifyCallback={verifyCallback}
+          ></ReCaptcha>
+        </div>
+
         <div className="form-footer">
-          <p>Don't have an account? Get started</p>
+          <p>
+            Don't have an account? <Link to="/signup">Get started</Link>
+          </p>
         </div>
       </div>
     </div>
