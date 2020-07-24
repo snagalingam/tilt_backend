230a396b723b40c338123664e82a9e647ef39595
Tony Yu <toekneeyou@tonys-mbp.lan>
2020-07-23 22:08:02 -0700

    skip onboarding view

---
+++ frontend/src/views/onboarding/skipOnboarding/SkipOnboarding.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+
+const SkipOnboarding = ({ firstName, next }) => {
+  const [isSkip, setIsSkip] = useState(false);
+
+  if (isSkip) return <Redirect push to="/dashboard" />;
+
+  return (
+    <div className="skip-onboarding-container form-container">
+      <div className="form-header">
+        <h1>{`Hi, ${firstName || "default"}`}</h1>
+        <p>
+          To help personalize your experience, we need a bit more information
+        </p>
+      </div>
+      <button onClick={next}>Let's Start</button>
+      <button className="secondary-button" onClick={() => setIsSkip(true)}>
+        Skip for Now
+      </button>
+    </div>
+  );
+};
+
+export default SkipOnboarding;
