fe41b55c6bfbdaec269ed27a486c925a1ef495db
Tony Yu <toekneeyou@tonys-mbp.lan>
2020-07-23 22:59:15 -0700

    fixed logic for previous function

--- frontend/src/views/onboarding/preferredPronoun/PreferredPronoun.jsx
+++ frontend/src/views/onboarding/preferredPronoun/PreferredPronoun.jsx
@@ -1,10 +1,39 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 
-const PreferredPronoun = ({}) => {
+const PreferredPronoun = ({
+  answers,
+  userTypes,
+  highSchool,
+  college,
+  parent,
+  counselor,
+  previous,
+}) => {
+  const { HIGH_SCHOOL, COLLEGE, PARENT, COUNSELOR } = userTypes;
+  const { userType } = answers;
+
+  function goBack() {
+    if (userType === HIGH_SCHOOL) {
+      previous(highSchool);
+    }
+    if (userType === COLLEGE) {
+      previous(college);
+    }
+    if (userType === PARENT) {
+      previous(parent);
+    }
+    if (userType === COUNSELOR) {
+      previous(counselor);
+    }
+  }
+
   return (
     <div className="preferred-pronoun-container form-container">
       Preferred Pronoun
+      <button className="secondary-button" onClick={goBack}>
+        Back
+      </button>
     </div>
   );
 };
