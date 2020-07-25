66ec2a10e1e213379fafd7a3fa613208db34e86b
Tony Yu <toekneeyou@tonys-mbp.lan>
2020-07-25 00:41:37 -0700

    parent flow complete

--- frontend/src/views/onboarding/parent/Parent.jsx
+++ frontend/src/views/onboarding/parent/Parent.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 
-const Parent = ({ next, flowIndex, setAnswers }) => {
-  return <div className="parent-container form-container">Parent</div>;
+import HighSchoolName from "../highSchoolName/HighSchoolName";
+import GradYear from "../graduationYear/GradYear";
+
+const Parent = (props) => {
+  const { flowIndex } = props;
+  return (
+    <div className="parent-container">
+      {flowIndex === 1 && <HighSchoolName {...props} parent />}
+      {flowIndex === 2 && <GradYear {...props} parent />}
+    </div>
+  );
 };
 
 export default Parent;
