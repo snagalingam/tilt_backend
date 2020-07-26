5738c46ee3cbf0f12c830569d7e0f374fe2aeca1
Tony Yu <toekneeyou@tonys-mbp.lan>
2020-07-26 11:15:13 -0700

    finished navbar styling

---
+++ frontend/src/components/banner/Banner.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import * as yup from "yup";
+
+import banner1 from "../../img/banner1.png";
+import "./banner.scss";
+
+const emailSchema = yup.object().shape({
+  email: yup.string().email().required("Please enter a valid email"),
+});
+
+const Banner = () => {
+  function handleSubmit(values) {
+    console.log(values);
+  }
+
+  return (
+    <div className="tilt-banner-container">
+      <div className="left">
+        <h1>Make smart financial decisions when choosing a college</h1>
+        <p>
+          We provide friendly reports simplifying your financial aid packages
+          and helping you compare across colleges
+        </p>
+        <div>
+          <Formik
+            initialValues={{
+              email: "",
+            }}
+            validationSchema={emailSchema}
+            onSubmit={handleSubmit}
+          >
+            {(state) => (
+              <Form className="tilt-email-input">
+                <Field name="email" placeholder="Enter your email"></Field>
+                {/* {state.errors && state.touched && (
+            <span className="custom-text-input-error">
+              {state.errors[field]}
+            </span>
+          )} */}
+                <button className="tilt-button dark" type="submit">
+                  Get Started
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </div>
+      <div className="right">
+        <img src={banner1} alt="girl waiting" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
