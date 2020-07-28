1ce76f8f63670e0b784a392435dce8911de15569
Tony Yu <toekneeyou@tonys-mbp.lan>
2020-07-28 11:55:38 -0700

    updated accordion styling

--- frontend/src/components/tiltAccordion/TiltAccordion.jsx
+++ frontend/src/components/tiltAccordion/TiltAccordion.jsx
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "@material-ui/core/Accordion";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import AccordionDetails from "@material-ui/core/AccordionDetails";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import RemoveCircleIcon from "@material-ui/icons/RemoveCircle";
 
 import "./tilt-accordion.scss";
 
-const TiltAccordion = ({ preview, content }) => {
+const ExpandIcon = ({ isExpanded, handleClick }) => {
   return (
-    <Accordion className="tilt-accordion" square={true}>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls={`${preview}-content`}
-      >
-        {preview}
+    <div className="expand-icon" onClick={handleClick}>
+      {isExpanded ? (
+        <RemoveCircleIcon className="custom" />
+      ) : (
+        <AddCircleIcon className="custom" />
+      )}
+    </div>
+  );
+};
+
+const TiltAccordion = ({ preview, content }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <Accordion
+      className="tilt-accordion"
+      square={true}
+      expanded={isExpanded}
+      onChange={() => setIsExpanded(!isExpanded)}
+    >
+      <AccordionSummary aria-controls={`${preview}-content`}>
+        {preview} <ExpandIcon isExpanded={isExpanded} />
       </AccordionSummary>
       <AccordionDetails>{content}</AccordionDetails>
     </Accordion>
