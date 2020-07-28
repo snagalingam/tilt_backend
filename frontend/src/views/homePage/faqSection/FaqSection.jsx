1572ddff5f2bb06c7b4057e585fa6c739ef7158d
Tony Yu <toekneeyou@tonys-mbp.lan>
2020-07-27 19:03:47 -0700

    updated social media links

--- frontend/src/views/homePage/faqSection/FaqSection.jsx
+++ frontend/src/views/homePage/faqSection/FaqSection.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-import Accordion from "@material-ui/core/Accordion";
-import AccordionSummary from "@material-ui/core/AccordionSummary";
-import AccordionDetails from "@material-ui/core/AccordionDetails";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
 import TiltAccordion from "../../../components/tiltAccordion/TiltAccordion";
 
 import "./faq-section.scss";
