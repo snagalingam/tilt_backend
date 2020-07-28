6bfd20df6e118453d3aaff7d383a522d79375fa5
Tony Yu <toekneeyou@tonys-mbp.lan>
2020-07-27 21:26:19 -0700

    fixed responsiveness of faq section

--- frontend/src/views/homePage/faqSection/FaqSection.jsx
+++ frontend/src/views/homePage/faqSection/FaqSection.jsx
@@ -7,38 +7,39 @@ import "./faq-section.scss";
 const FaqSection = () => {
   return (
     <div className="tilt-faq">
-      <h2>Frequently Asked Questions</h2>
-      <div>
+      <div className="faq-container">
+        <h2>Frequently Asked Questions</h2>
         <div>
-          <TiltAccordion
-            preview="What is financial aid?"
-            content="Yes, we are commited to helping students access financial aid free-of-charge. We only charge schools and organizations for additional services to help their students."
-          />
-          <TiltAccordion
-            preview="Do I qualify for financial aid?"
-            content="Yes, we are commited to helping students access financial aid free-of-charge. We only charge schools and organizations for additional services to help their students."
-          />
-          <TiltAccordion
-            preview="Don't only students with good grades receive financial aid?"
-            content="Yes, we are commited to helping students access financial aid free-of-charge. We only charge schools and organizations for additional services to help their students."
-          />
-        </div>
-        <div>
-          <TiltAccordion
-            preview="How does financial aid work?"
-            content="Yes, we are commited to helping students access financial aid free-of-charge. We only charge schools and organizations for additional services to help their students."
-          />
-          <TiltAccordion
-            preview={`What if my parents make "too much money"?`}
-            content="Yes, we are commited to helping students access financial aid free-of-charge. We only charge schools and organizations for additional services to help their students."
-          />
-          <TiltAccordion
-            preview="Will this service always be free?"
-            content="Yes, we are commited to helping students access financial aid free-of-charge. We only charge schools and organizations for additional services to help their students."
-          />
+          <div>
+            <TiltAccordion
+              preview="What is financial aid?"
+              content="Yes, we are commited to helping students access financial aid free-of-charge. We only charge schools and organizations for additional services to help their students."
+            />
+            <TiltAccordion
+              preview="Do I qualify for financial aid?"
+              content="Yes, we are commited to helping students access financial aid free-of-charge. We only charge schools and organizations for additional services to help their students."
+            />
+            <TiltAccordion
+              preview="Don't only students with good grades receive financial aid?"
+              content="Yes, we are commited to helping students access financial aid free-of-charge. We only charge schools and organizations for additional services to help their students."
+            />
+          </div>
+          <div>
+            <TiltAccordion
+              preview="How does financial aid work?"
+              content="Yes, we are commited to helping students access financial aid free-of-charge. We only charge schools and organizations for additional services to help their students."
+            />
+            <TiltAccordion
+              preview={`What if my parents make "too much money"?`}
+              content="Yes, we are commited to helping students access financial aid free-of-charge. We only charge schools and organizations for additional services to help their students."
+            />
+            <TiltAccordion
+              preview="Will this service always be free?"
+              content="Yes, we are commited to helping students access financial aid free-of-charge. We only charge schools and organizations for additional services to help their students."
+            />
+          </div>
         </div>
       </div>
-      <div></div>
     </div>
   );
 };
