9ea2c4b9755689b1b8214a18372d1ad92952a57b
Tony Yu <toekneeyou@tonys-mbp.lan>
2020-07-29 19:56:08 -0700

    updated styling for current page in navbar

--- frontend/src/components/navBar/NavBar.jsx
+++ frontend/src/components/navBar/NavBar.jsx
@@ -1,34 +1,51 @@
 import React from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link, useHistory, useLocation } from "react-router-dom";
 
 import TiltButton from "../tiltButton/TiltButton";
 
 import tiltLogo from "../../img/tilt_logo.png";
 import "./nav-bar.scss";
 
+const links = [
+  {
+    display: "Blog",
+    pathname: "/blog",
+  },
+  {
+    display: "FAQ",
+    pathname: "/faq",
+  },
+  {
+    display: "Contacts",
+    pathname: "/contacts",
+  },
+];
+
 const NavBar = () => {
   const history = useHistory();
+  const location = useLocation();
+  const { pathname } = location;
+  console.log(pathname);
 
   return (
     <div className="tilt-nav-bar">
-      <img src={tiltLogo} alt="tilt logo" />
+      <Link to="/">
+        <img src={tiltLogo} alt="tilt logo" />
+      </Link>
       <nav>
         <ul>
-          <li>
-            <Link className="tilt-button" to="/blog">
-              Blog
-            </Link>
-          </li>
-          <li>
-            <Link className="tilt-button" to="/faq">
-              FAQ
-            </Link>
-          </li>
-          <li>
-            <Link className="tilt-button" to="/contacts">
-              Contacts
-            </Link>
-          </li>
+          {links.map((link) => {
+            const selected = pathname === link.pathname;
+            return (
+              <li
+                key={link.pathname}
+                className={`${selected ? "selected" : ""}`}
+              >
+                <Link to={link.pathname}>{link.display}</Link>
+                <div className="underline" />
+              </li>
+            );
+          })}
         </ul>
       </nav>
       <div className="login-and-get-started">
