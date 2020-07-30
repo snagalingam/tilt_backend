f6f52b5e7606110a5487ff50d8fac4a203455bd9
Tony Yu <toekneeyou@tonys-mbp.lan>
2020-07-29 20:32:46 -0700

    faq skeleton complete

--- frontend/src/views/faqPage/FaqPage.jsx
+++ frontend/src/views/faqPage/FaqPage.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+
+import FaqCard from "./faqCard/FaqCard";
+import TiltAccordion from "../../components/tiltAccordion/TiltAccordion";
+import Footer from "../../components/footer/Footer";
+
+import "./faq-page.scss";
 
 function FaqPage() {
-    return <div className='faq-page-container'>
-
+  return (
+    <div className="faq-page-container">
+      <h1>How Can We Help You?</h1>
+      <div className="faq-search">
+        <input placeholder="Search for questions" />
+        <div>
+          <SearchIcon className="search-icon" />
+        </div>
+      </div>
+      <div className="faq-cards">
+        <FaqCard />
+        <FaqCard />
+        <FaqCard />
+        <FaqCard />
+      </div>
+      <div className="faq-accordions">
+        <div>
+          <TiltAccordion preview="What is financial aid?" content="money" />
+          <TiltAccordion preview="What is financial aid?" content="money" />
+          <TiltAccordion preview="What is financial aid?" content="money" />
+          <TiltAccordion preview="What is financial aid?" content="money" />
+        </div>
+        <div>
+          <TiltAccordion preview="What is financial aid?" content="money" />
+          <TiltAccordion preview="What is financial aid?" content="money" />
+          <TiltAccordion preview="What is financial aid?" content="money" />
+          <TiltAccordion preview="What is financial aid?" content="money" />
+        </div>
+      </div>
+      <div className="faq-footer">
+        <Footer />
+      </div>
     </div>
+  );
 }
 
 export default FaqPage;
