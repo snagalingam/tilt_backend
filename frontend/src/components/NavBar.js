d2b03790f887f5bbc19920dd9b1bf0a80a7e3d9a
Tony Yu <toekneeyou@tonys-mbp.lan>
2020-07-29 19:56:08 -0700

    updated styling for current page in navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,58 +1,90 @@
-import React, {useState } from 'react';
-import { Link } from 'react-router-dom';
-import { X } from 'react-feather';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { X } from "react-feather";
 
-import logo from '../img/tilt_logo.png';
+import logo from "../img/tilt_logo.png";
 
-
-const containerFluid = type => {
-  if (type === 'boxed') {
-    return 'container';
+const containerFluid = (type) => {
+  if (type === "boxed") {
+    return "container";
   }
-  return 'container-fluid';
+  return "container-fluid";
 };
 
-const navStyle = classList => {
-  var base = "navbar navbar-expand-lg"
+const navStyle = (classList) => {
+  var base = "navbar navbar-expand-lg";
   if (classList) {
     return base.concat(classList);
   }
   return base;
 };
 
-const NavBar = props => {
+const NavBar = (props) => {
   const classList = props.classList;
   const type = props.type;
   const [collapsed, setCollapsed] = useState(true);
   const toggleNavbar = () => setCollapsed(!collapsed);
-  const classNavBarCollapsed = collapsed ? 'collapse navbar-collapse' : 'collapse navbar-collapse show';
-  const classNavBarToggler = collapsed ? 'navbar-toggler collapsed' : 'navbar-toggler';
+  const classNavBarCollapsed = collapsed
+    ? "collapse navbar-collapse"
+    : "collapse navbar-collapse show";
+  const classNavBarToggler = collapsed
+    ? "navbar-toggler collapsed"
+    : "navbar-toggler";
 
   return (
     <nav className={navStyle(classList)}>
       <div className={containerFluid(type)}>
-
         {/* Brand */}
         <Link className="navbar-brand" to="/">
           <img src={logo} className="navbar-brand-img" alt="..." />
         </Link>
 
         {/* Toggler */}
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarGetStarted" aria-controls="navbarGetStarted" aria-expanded="false" aria-label="Toggle Get Started">
-          <a className="btn btn-sm btn-primary lift ml-auto" href="https://kellogg.qualtrics.com/jfe/form/SV_578NDO4HdsYbd4h" target="_blank" rel="noopener noreferrer">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarGetStarted"
+          aria-controls="navbarGetStarted"
+          aria-expanded="false"
+          aria-label="Toggle Get Started"
+        >
+          <a
+            className="btn btn-sm btn-primary lift ml-auto"
+            href="https://kellogg.qualtrics.com/jfe/form/SV_578NDO4HdsYbd4h"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Get Started
           </a>
         </button>
 
-        <button className={classNavBarToggler} type="button" onClick={toggleNavbar} data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          className={classNavBarToggler}
+          type="button"
+          onClick={toggleNavbar}
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
 
         {/* Collapse */}
         <div className={classNavBarCollapsed} id="navbarCollapse">
-
           {/* Toggler */}
-          <button className="navbar-toggler" type="button" onClick={toggleNavbar} data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            onClick={toggleNavbar}
+            data-toggle="collapse"
+            data-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <X className="fe" />
           </button>
 
@@ -71,12 +103,13 @@ const NavBar = props => {
             </li>
           </ul>
 
-
           {/* Button */}
-          <Link className="navbar-btn btn btn-sm btn-primary lift ml-auto" to="/signup-survey">
+          <Link
+            className="navbar-btn btn btn-sm btn-primary lift ml-auto"
+            to="/signup-survey"
+          >
             Get Started
           </Link>
-
         </div>
       </div>
     </nav>
