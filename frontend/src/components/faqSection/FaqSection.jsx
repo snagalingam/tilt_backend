09efeb09a870adab7dcc691bece986b81360b819
Tony Yu <toekneeyou@tonys-mbp.lan>
2020-07-31 16:59:09 -0700

    finished styling of faq and contacts

--- frontend/src/components/faqSection/FaqSection.jsx
+++ frontend/src/components/faqSection/FaqSection.jsx
@@ -1,45 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import TiltAccordion from "../tiltAccordion/TiltAccordion";
+import { faqs } from "../../helper/faqs";
 
 import "./faq-section.scss";
 
-const FaqSection = () => {
+function FaqSection() {
+  const [columnOne, setColumnOne] = useState([]);
+  const [columnTwo, setColumnTwo] = useState([]);
+
+  const divideFaqs = () => {
+    const one = [];
+    const two = [];
+    for (let i = 0; i < faqs.length; i++) {
+      const { preview, content } = faqs[i];
+      if (i % 2 === 0) {
+        one.push(
+          <TiltAccordion key={preview} preview={preview} content={content} />
+        );
+      } else {
+        two.push(
+          <TiltAccordion key={preview} preview={preview} content={content} />
+        );
+      }
+    }
+    setColumnOne((prev) => [...prev, ...one]);
+    setColumnTwo((prev) => [...prev, ...two]);
+  };
+
+  useEffect(() => {
+    divideFaqs();
+  }, []);
+
   return (
-    <div className="tilt-faq">
+    <div className="FaqSection">
       <h2>Frequently Asked Questions</h2>
-      <div>
-        <div>
-          <TiltAccordion
-            preview="What is financial aid?"
-            content="Financial aid is any form of funding that helps you pay for college, including scholarships, grants, loans and work-study programs."
-          />
-          <TiltAccordion
-            preview="Do I qualify for financial aid?"
-            content="To qualify for financial aid, you have to be a U.S. citizen or eligible noncitizen and be enrolled in a degree or certificate program at your college or career school. That’s it! If you are an undocumented student, you might be able to apply for state financial aid."
-          />
-          <TiltAccordion
-            preview="Don't only students with good grades receive financial aid?"
-            content="That is a common misconception. You can receive financial aid for a variety of reasons, and good grades is only one reason. No matter what your grades are, you should always apply for financial aid."
-          />
-        </div>
-        <div>
-          <TiltAccordion
-            preview="How does financial aid work?"
-            content="To receive financial aid, you must submit financial aid forms, generally the FAFSA (Free Application for Federal Student Aid). Once you have been accepted to a college, they will send you a financial aid package. You can appeal and negotiate for more funds if you would like. When school starts, the financial aid is used to pay your student bills first, and then, you can take the leftover money to pay the rest of your expenses."
-          />
-          <TiltAccordion
-            preview={`What if my parents make "too much money"?`}
-            content="The reality is there’s no income cut-off to qualify for federal student aid. It doesn’t matter if you have a low or high income, you will still qualify for some type of financial aid, including low-interest student loans."
-          />
-          <TiltAccordion
-            preview="Will this service always be free?"
-            content="Yes, we are committed to helping students and families access financial aid free-of-charge. We only charge schools and organizations for additional services to help their students."
-          />
-        </div>
+      <div className="FaqSection_columns">
+        <div className="FaqSection_column-1">{columnOne}</div>
+        <div className="FaqSection_column-2">{columnTwo}</div>
       </div>
     </div>
   );
-};
+}
 
 export default FaqSection;
--- frontend/src/views/faqPage/FaqPage.jsx
+++ frontend/src/views/faqPage/FaqPage.jsx
@@ -2,64 +2,36 @@ import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
 
 import FaqCard from "./faqCard/FaqCard";
+import FaqSection from "../../components/faqSection/FaqSection";
 import TiltAccordion from "../../components/tiltAccordion/TiltAccordion";
 import Footer from "../../components/footer/Footer";
 
 import "./faq-page.scss";
 
 function FaqPage() {
+  const searchFunction = (value) => {};
+
   return (
-    <div className="faq-page-container view-container">
-      <div>
+    <div className="FaqPage view-container">
+      <div className="FaqPage__header">
         <h2>How Can We Help You?</h2>
       </div>
-      <div className="faq-search">
+      <div className="FaqPage__search">
         <input placeholder="Search for questions" />
         <div>
           <SearchIcon className="search-icon" />
         </div>
       </div>
-      <div className="faq-cards">
+      <div className="FaqPage__cards">
         <FaqCard />
         <FaqCard />
         <FaqCard />
         <FaqCard />
       </div>
-      <div className="faq-accordions">
-        <div>
-          <TiltAccordion
-            preview="What is financial aid?"
-            content="Financial aid is any form of funding that helps you pay for college, including scholarships, grants, loans and work-study programs."
-          />
-          <TiltAccordion
-            preview="What’s the difference between loans, grants, and scholarships?"
-            content="A scholarship is a money-based award specifically designated for education-related expenses. Grants are gift aid awarded to students often based on financial need. They typically come from state government, federal government, non-profit organizations, and schools. A student loan is a type of loan designed to help students pay for post-secondary education. Students must pay back the loan."
-          />
-          <TiltAccordion
-            preview="My parents make “too much money: I won’t qualify."
-            content="The reality is there’s no income cut-off to qualify for federal student aid. It doesn’t matter if you have a low or high income, you will still qualify for some type of financial aid, including low-interst student loans."
-          />
-          <TiltAccordion
-            preview="What can I get from financial aid?"
-            content="Financial aid helps students and their families pay for college by covering higher education expenses, such as tuition and fees, room and board, books and supplies, and transportation."
-          />
-        </div>
-        <div>
-          <TiltAccordion
-            preview="Do I qualify for financial aid?"
-            content="Requirements includ that you have financial need, are a U.S. citizen or eligible noncitizen, be enrolled in an eligible degree or certificate program at your college or career school, and more."
-          />
-          <TiltAccordion
-            preview="Subsidized vs. Unsubsidized loans?"
-            content="money"
-          />
-          <TiltAccordion
-            preview="Only students with good grades get financial aid."
-            content="While a high grade point average will help a student get into a good school and may help with academic scholarships, most of the federal student aid programs do not take a student’s grades into consideration."
-          />
-        </div>
-      </div>
-      <div className="faq-footer">
+
+      <FaqSection />
+
+      <div className="FaqPage__footer">
         <Footer />
       </div>
     </div>
