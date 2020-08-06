e45b21227b7327784cd220000c56b4a0bbbba50e
Tony Yu <toekneeyou@tonys-mbp.lan>
2020-08-05 22:56:58 -0700

    refactor

--- backend/users/send_email.py
+++ backend/users/send_email.py
@@ -11,6 +11,10 @@ import datetime
 # Template ID: d-274ce0ccdabc445eb7c488c7c98695e6
 
 ENVIRONMENT = os.environ.get('ENVIRONMENT', default='production')
+if ENVIRONMENT == 'development':
+    domain = "http://localhost:3000"
+elif ENVIRONMENT == 'production':
+    domain = 'https://tilt-next.vercel.app'
 
 
 def send_verification(email, first_name):
@@ -23,10 +27,7 @@ def send_verification(email, first_name):
                        os.environ.get('SECRET_KEY'),
                        algorithm='HS256',
                        headers={'domain': 'www.tiltaccess.com'}).decode('utf-8')
-    if ENVIRONMENT == 'development':
-        url = f"http://localhost:3000/activate/{token}"
-    if ENVIRONMENT == 'production':
-        url = f"https://tilt-next.vercel.app/activate/{token}"
+    url = f"{domain}/activate/{token}"
 
     message.template_id = 'd-274ce0ccdabc445eb7c488c7c98695e6'
     message.dynamic_template_data = {
@@ -60,10 +61,7 @@ def send_reset_password(email, first_name):
                        algorithm='HS256',
                        headers={'domain': 'www.tiltaccess.com'}).decode('utf-8')
 
-    if ENVIRONMENT == 'development':
-        url = f"http://localhost:3000/forgot-password/{token}"
-    if ENVIRONMENT == 'production':
-        url = f"https://tilt-next.vercel.app/forgot-password/{token}"
+    url = f"{domain}/forgot-password/{token}"
 
     message.template_id = 'd-721a69f0688d484db91503c611d87d1c'
     message.dynamic_template_data = {
