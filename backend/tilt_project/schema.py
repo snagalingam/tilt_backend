b4bc58bae32cf0982a5ccdb3b47a4a8c190dfb5b
Tony Yu <toekneeyou@tonys-mbp.lan>
2020-08-11 12:06:56 -0700

    danny's changes

--- backend/tilt_project/schema.py
+++ backend/tilt_project/schema.py
@@ -3,21 +3,27 @@ import graphql_jwt
 
 import scholarships.schema
 import users.schema
+import organizations.schema
+
 
 class Query(
     scholarships.schema.Query,
     users.schema.Query,
+    organizations.schema.Query,
     graphene.ObjectType
 ):
     pass
 
+
 class Mutation(
     scholarships.schema.Mutation,
     users.schema.Mutation,
+    organizations.schema.Mutation,
     graphene.ObjectType
 ):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
-    refersh_token = graphql_jwt.Refresh.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
