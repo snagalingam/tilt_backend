5dcfb8639a6393c99d04f4aa33be31d80e117f4d
Danny Yoo <dansteryoo@gmail.com>
2020-08-28 17:48:13 -0400

    uuid, favicon, college_search, college table

---
+++ backend/colleges/models.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+
+class College(models.Model):
+
+    # college scorecard info
+    college_scorecard_id = models.IntegerField(blank=True, null=True)
+
+    # google api inputted
+    place_id = models.CharField(max_length=250, blank=True, null=True)
+    business_status = models.CharField(max_length=25, blank=True, null=True)
+    name = models.CharField(max_length=250, blank=True, null=True)
+    address = models.CharField(max_length=250, blank=True, null=True)
+    phone_number = models.CharField(max_length=25, null=True, blank=True)
+    lat = models.CharField(max_length=250, null=True, blank=True)
+    lng = models.CharField(max_length=250, null=True, blank=True)
+    url = models.CharField(max_length=250, null=True, blank=True)
+    website = models.CharField(max_length=250, null=True, blank=True)
+    favicon = models.CharField(max_length=250, null=True, blank=True)
+    main_photo = models.CharField(max_length=250, null=True, blank=True)
+    photos = ArrayField(
+        models.CharField(max_length=250, null=True, blank=True),
+        null=True, default=None
+    )
+    types = ArrayField(
+        models.CharField(max_length=250, null=True, blank=True),
+        null=True, default=None
+    )
+
+    # automatically added
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
