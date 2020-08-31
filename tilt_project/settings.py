f99f6ddb0550dae78d5f952a72686ce7c44522c9
Sinthuja Nagalingam <sinthu.n@gmail.com>
2020-08-30 21:17:43 -0700

    another try

--- tilt_project/settings.py
+++ tilt_project/settings.py
@@ -229,22 +229,23 @@ DATABASES['default'].update(db_from_env)
 SESSION_COOKIE_SAMESITE = None
 CSRF_COOKIE_SAMESITE = None
 CSRF_TRUSTED_ORIGINS = [
-    "https://tiltaccess.com",
-    "https://www.tiltaccess.com",
+    "tiltaccess.com",
+    "www.tiltaccess.com",
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://tiltaccess.com",
-    "https://www.tiltaccess.com",
+    "tiltaccess.com",
+    "www.tiltaccess.com",
 ]
+CORS_EXPOSE_HEADERS = [
 
+]
 # security for development
 if ENVIRONMENT == 'development':
     CORS_ORIGIN_ALLOW_ALL = True
 # security for production
 if ENVIRONMENT == 'production':
     CORS_ORIGIN_ALLOW_ALL = False
-    CORS_ORIGIN_ALLOW = True
     CSRF_COOKIE_SECURE = True
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
