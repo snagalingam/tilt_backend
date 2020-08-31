72a73538fa34b3c848fdca49e2eb208d73d0c65c
Danny Yoo <dansteryoo@gmail.com>
2020-08-31 17:41:02 -0400

    college data seeding

---
+++ colleges/data/_README.md
@@ -0,0 +1,7 @@
+# How to seed in django 
+
+1. Run
+    `python manage.py loaddata tilt_v3 --format=json --indent=2 > tilt_v3/fixtures/colleges_seeds.json`
+
+2. Fields
+    `Unit ID, PE ID, Name, City, Zipcode`
--- services/helpers/fav_finder.py
+++ services/helpers/fav_finder.py
@@ -4,11 +4,14 @@ import favicon
 
 def get_favicon(url):
 
-    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-    headers = {'User-Agent': user_agent}
-    icons = favicon.get(f"{url}", headers=headers, timeout=2)
-    
-    print(icons[0].url)
-    return icons[0].url
+    try:
+        user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        headers = {'User-Agent': user_agent}
+        icons = favicon.get(f"{url}", headers=headers, timeout=2)
+    except:
+        return ''
 
-# /tmp/python-favicon.png
+    try:
+        return icons[0].url
+    except:
+        return ''
