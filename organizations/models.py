6129b9aeace77f501a60b513d4cbd575c6054cbd
Danny Yoo <dansteryoo@gmail.com>
2020-09-16 12:33:27 -0400

    restore

--- organizations/models.py
+++ organizations/models.py
@@ -24,3 +24,6 @@ class Organization(models.Model):
     # automatically added
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
