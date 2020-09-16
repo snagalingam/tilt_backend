eb6dcd228a6d0ef58e1290b1e9e9d52b7abf514e
Danny Yoo <dansteryoo@gmail.com>
2020-09-16 13:07:12 -0400

    update

--- my_colleges/models.py
+++ my_colleges/models.py
@@ -6,32 +6,13 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 
-class College(models.Model):
+class MyCollege(models.Model):
 
-    # college scorecard info
-    unit_id = models.CharField(max_length=255, blank=True, null=True)
-    ope_id = models.CharField(max_length=255, blank=True, null=True)
+    user_id = models.CharField(max_length=255, blank=True, null=True)
+    college_id = models.CharField(max_length=255, blank=True, null=True)
 
-    # google api inputted
-    place_id = models.CharField(max_length=255, blank=True, null=True)
-    business_status = models.CharField(max_length=255, blank=True, null=True)
-    name = models.CharField(max_length=255, blank=True, null=True)
-    address = models.CharField(max_length=255, blank=True, null=True)
-    phone_number = models.CharField(max_length=255, null=True, blank=True)
-    lat = models.IntegerField(null=True, blank=True)
-    lng = models.IntegerField(null=True, blank=True)
-    url = models.TextField(null=True, blank=True)
-    website = models.TextField(null=True, blank=True)
-    favicon = models.TextField(null=True, blank=True)
-    main_photo = models.TextField(null=True, blank=True)
-    photos = ArrayField(
-        models.TextField(null=True, blank=True),
-        null=True, default=None
-    )
-    types = ArrayField(
-        models.CharField(max_length=255, null=True, blank=True),
-        null=True, default=None
-    )
+    college_status = models.CharField(max_length=255, blank=True, null=True)
+    net_price = models.IntegerField(blank=True, null=True)
 
     # automatically added
     created = models.DateTimeField(auto_now_add=True, null=True)
