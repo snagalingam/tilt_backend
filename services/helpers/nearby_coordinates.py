10600139ed47342ec1d0d25775d679c40be16695
Danny Yoo <dansteryoo@gmail.com>
2020-10-08 17:23:22 -0400

    update by lat, lng, zipcode, city, state for nearby_colleges

--- services/helpers/nearby_coordinates.py
+++ services/helpers/nearby_coordinates.py
@@ -1,13 +1,15 @@
 from math import radians, cos, sin, asin, sqrt
 from uszipcode import SearchEngine
 
+
 def check_distance(center_lat, center_lng, test_lat, test_lng):
     """
     Calculate the great circle distance between two points 
     on the earth (specified in decimal degrees)
     """
     # convert decimal degrees to radians
-    center_lat, center_lng, test_lat, test_lng = map(radians, [center_lat, center_lng, test_lat, test_lng])
+    center_lat, center_lng, test_lat, test_lng = map(
+        radians, [center_lat, center_lng, test_lat, test_lng])
 
     # haversine formula
     dlon = test_lng - center_lng
@@ -18,10 +20,31 @@ def check_distance(center_lat, center_lng, test_lat, test_lng):
     distance = c * r
     return distance
 
-def state_from_zipcode(zipcode):
+
+def check_by_city(city, state):
+    search = SearchEngine(simple_zipcode=True)
+    city = search.by_city_and_state(city=city, state=state)
+    result = city[0].to_dict()
+    state = result.get("state")
+    lat = result.get("lat")
+    lng = result.get("lng")
+    return [state, lat, lng]
+
+
+def check_by_zipcode(zipcode):
     search = SearchEngine(simple_zipcode=True)
     zipcode = search.by_zipcode(zipcode)
     result = zipcode.to_dict()
+    state = result.get("state")
+    lat = result.get("lat")
+    lng = result.get("lng")
+    return [state, lat, lng]
+
+
+def check_by_coordinates(lat, lng):
+    search = SearchEngine(simple_zipcode=True)
+    coordinates = search.by_coordinates(lat, lng, radius=25)
+    result = coordinates[0].to_dict()
     state = result.get("state")
     lat = result.get("lat")
     lng = result.get("lng")
