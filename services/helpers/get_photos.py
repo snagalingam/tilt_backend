411aeae522687ab6f46095d4fe31f614592280e3
Danny Yoo <dansteryoo@gmail.com>
2020-10-27 17:07:03 -0400

    add get_photos script

---
+++ services/helpers/get_photos.py
@@ -0,0 +1,51 @@
+import os
+import json
+import requests
+from urllib.parse import urlencode
+
+def get_photo_url(file_name):
+    colleges = json.load(open(f'{file_name}.json'))
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit537.36(KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    urls = []
+
+    for college in colleges[0:]:
+        pk = college.get('pk')
+        fields = college.get('fields')
+        place_id = fields.get('place_id')
+        name = fields.get('name')
+        photos = fields.get('photos')
+        main_photo = fields.get('main_photo')
+
+        if len(main_photo) > 0:
+            try:
+                response = requests.get(main_photo, headers=headers)
+                main_photo_url = response.url
+            except Exception as e:
+                print(e)
+                breakpoint()
+        else:
+            main_photo_url = main_photo
+
+        photo_urls = []
+        if len(photos) > 0:
+            for p in photos: 
+                res = requests.get(p, headers=headers)
+                photo_urls.append(res.url)
+
+        data = {
+            "place_id": place_id,
+            "name": name,
+            "main_photo": main_photo_url,
+            "photos": photo_urls,
+        }
+        urls.append(data)
+
+        print(f'  ==> pk: {pk} / name: {name}')
+        with open(f'photo_urls.json', 'a+') as f:
+            scorecard_data = json.dumps(data, indent=2, ensure_ascii=False)
+            f.write(scorecard_data + ',')
+
+    return print(f'  ==> FINISHED: {file_name}')
+
+# get_photo_url('colleges_seeds')
