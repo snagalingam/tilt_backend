0739503cac65a2b2c20b35744cefbcab727f3275
Tony Yu <tonyyu1129@hotmail.com>
2020-11-21 10:23:40 -0800

    fix admissions rate sort

--- colleges/schema.py
+++ colleges/schema.py
@@ -223,13 +223,13 @@ class Query(graphene.ObjectType):
                 else:
                     qs = qs.annotate(real_net_price=Least('collegestatus__net_price',
                                                           'scorecard__avg_net_price')).order_by(F('real_net_price').desc(nulls_last=True))
-
         if sort_by == "admissions_rate":
             if sort_order == 'asc':
                 qs = qs.order_by(
                     F("scorecard__admissions_rate").asc(nulls_last=True))
             else:
-                qs = qs.order_by('-scorecard__admissions_rate')
+                qs = qs.order_by(
+                    F("scorecard__admissions_rate").desc(nulls_last=True))
         if sort_by == "city":
             qs = qs.order_by('scorecard__city' if sort_order ==
                              'asc' else '-scorecard__city')
