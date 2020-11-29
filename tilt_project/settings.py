6c4c5f92737b5db268bf9ff5029d52443c4d9337
Danny Yoo <dansteryoo@gmail.com>
2020-11-29 18:29:51 -0500

    remove ssl=False

--- tilt_project/settings.py
+++ tilt_project/settings.py
@@ -239,7 +239,7 @@ GRAPHENE = {
 }
 
 # database
-db_from_env = dj_database_url.config(conn_max_age=500, ssl_require=False)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # security
