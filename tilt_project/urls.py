02eeb7502d28c90e4b689b668a4944947def1d64
Danny Yoo <dansteryoo@gmail.com>
2020-12-13 17:07:16 -0500

    update

--- tilt_project/urls.py
+++ tilt_project/urls.py
@@ -7,7 +7,7 @@ from django.views.generic import TemplateView
 from graphene_django.views import GraphQLView
 
 ENVIRONMENT = os.environ.get('ENVIRONMENT', default='production')
-TILT_APP = os.environ.get('TILT_APP')
+TILT_APP = os.environ.get('TILT_APP', None)
 
 if ENVIRONMENT == 'development' or TILT_APP == 'staging':
     urlpatterns = [
