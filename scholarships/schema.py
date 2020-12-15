dc8e1ddfe2dd85924fd0c526c0bbf146ed2bb329
Danny Yoo <dansteryoo@gmail.com>
2020-12-15 16:45:13 -0500

    on hold

--- scholarships/schema.py
+++ scholarships/schema.py
@@ -3,6 +3,7 @@ from graphene_django import DjangoObjectType
 from django.contrib.auth import get_user_model
 from .models import Provider, Scholarship, ScholarshipStatus
 from colleges.models import College
+import datetime
 
 class ProviderType(DjangoObjectType):
     class Meta:
@@ -41,6 +42,7 @@ class Query(graphene.ObjectType):
             provider_id=graphene.Int(),
             description=graphene.String(),
             website=graphene.String(),
+            deadline=graphene.Date(),
             max_amount=graphene.Int(),
             renewable=graphene.Boolean(),
             number_awards=graphene.Int(),
@@ -65,6 +67,9 @@ class Query(graphene.ObjectType):
             citizenship=graphene.List(graphene.String),
             first_generation=graphene.Boolean(),
             financial_need=graphene.Boolean())
+
+    # scholarship_by_user_criteria
+    scholarships_by_user_criteria = graphene.List(ScholarshipType)
 
     # scholarship_statuses
     scholarship_statuses_by_fields = graphene.List(
@@ -98,6 +103,134 @@ class Query(graphene.ObjectType):
     def resolve_scholarship_statuses_by_fields(self, info, **fields):
         qs = ScholarshipStatus.objects.filter(**fields)
         return qs
+
+    def resolve_scholarships_by_user_criteria(self, info):
+        qs = Scholarship.objects.all()
+        user = info.context.user
+
+#------ find education level 
+        current_date = datetime.datetime.today()
+        education_level = "college_student"
+
+        # if highschool grad year is less than current year 
+        if current_date.year - user.high_school_grad_year < 0:
+            education_level = "highschool_senior"
+
+        # if highschool grad year is current year but today is earlier than may
+        elif current_date.year - user.high_school_grad_year == 0:
+            if current_date.month < 6:
+                education_level = "highschool_senior"
+
+#------ find state from organization address
+        us_states = [
+            "AL",
+            "AK",
+            "AZ",
+            "AR",
+            "CA",
+            "CO",
+            "CT",
+            "DE",
+            "DC",
+            "FL",
+            "GA",
+            "HI",
+            "ID",
+            "IL",
+            "IN",
+            "IA",
+            "KS",
+            "KY",
+            "LA",
+            "ME",
+            "MD",
+            "MA",
+            "MI",
+            "MN",
+            "MS",
+            "MO",
+            "MT",
+            "NE",
+            "NV",
+            "NH",
+            "NJ",
+            "NM",
+            "NY",
+            "NC",
+            "ND",
+            "OH",
+            "OK",
+            "OR",
+            "PA",
+            "RI",
+            "SC",
+            "SD",
+            "TN",
+            "TX",
+            "UT",
+            "VT",
+            "VA",
+            "WA",
+            "WV",
+            "WI",
+            "WY"]
+
+        organization_address = user.organization.values()[0]["address"]
+        split_address = organization_address.split(" ")
+
+        for each in split_address:
+            if "," in each:
+                each = each.replace(",", "")
+            if each in us_states:
+                state = each
+
+#------ find ethnicity   
+        ethnicities = [
+            "american indian and alaska native",
+            "asian",
+            "black and african",
+            "hispanic and latinx",
+            "native hawaiian and pacific islander",
+            "white",
+            "other",
+        ]
+
+        ethnicity = user.ethnicity[0]
+        # multi_ethnic = []
+
+        # if len(user.ethnicity) > 1:
+        #     for i in range(len(user.ethnicity)):
+
+        #         ethnicity = user.ethnicity
+
+#------ find gender   
+        genders = {
+            "He/his": "men",
+            "She/hers": "women",
+            "They/theirs": "other"
+        }
+        gender = genders[user.pronouns]
+
+#------ find gpa and test scores  
+        user_gpa = user.gpa
+        user_sat = user.sat_math + user.sat_verbal
+        user_act = user.act_score
+
+        if education_level == "highschool_senior":
+            pass
+
+        if education_level == "college_student":
+            pass
+
+        qs = qs.filter()
+        # min_gpa =
+        # max_gpa =
+        # min_act = 
+        # min_sat = 
+
+        breakpoint()
+        return qs
+
 
 class CreateProvider(graphene.Mutation):
     provider = graphene.Field(ProviderType)
@@ -152,6 +285,7 @@ class CreateScholarship(graphene.Mutation):
         description = graphene.String()
         website = graphene.String()
         max_amount = graphene.Int()
+        deadline=graphene.Date()
         renewable = graphene.Boolean()
         number_awards = graphene.Int()
         education_level = graphene.List(graphene.String)
@@ -183,6 +317,7 @@ class CreateScholarship(graphene.Mutation):
         provider_id=None,
         description=None,
         website=None,
+        deadline=None,
         max_amount=None,
         renewable=None,
         number_awards=None,
@@ -218,6 +353,7 @@ class CreateScholarship(graphene.Mutation):
             description=description,
             website=website,
             max_amount=max_amount,
+            deadline=deadline,
             renewable=renewable,
             number_awards=number_awards,
             education_level=education_level,
