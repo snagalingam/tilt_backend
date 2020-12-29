8eb29e6d87bead0dbf168bff27d43d3448590f25
Sinthuja Nagalingam <sinthu.n@gmail.com>
2020-12-29 15:41:05 -0800

    updates to organization model

--- organizations/models.py
+++ organizations/models.py
@@ -6,22 +6,25 @@ DEFAULT_CHAR_TEXT = ""
 
 
 class Organization(models.Model):
+    # main information
     name = models.CharField(max_length=255)
     place_id = models.CharField(blank=True, max_length=255)
+    partner = models.BooleanField(default=False)
+
+    # additional fields
+    address = models.CharField(blank=True, max_length=255)
     business_status = models.CharField(blank=True, max_length=255)
     icon = models.CharField(blank=True, max_length=255)
-    address = models.CharField(blank=True, max_length=255)
-    phone_number = models.CharField(blank=True, max_length=255)
     lat = models.FloatField(blank=True, null=True)
     lng = models.FloatField(blank=True, null=True)
-    url = models.TextField(blank=True)
-    website = models.TextField(blank=True)
+    phone_number = models.CharField(blank=True, max_length=255)
     types = ArrayField(
         models.CharField(blank=True, max_length=255),
         blank=True,
         null=True
     )
-    partner = models.BooleanField(default=False)
+    url = models.TextField(blank=True)
+    website = models.TextField(blank=True)
 
     # automatically added
     created = models.DateTimeField(auto_now_add=True)
--- organizations/schema.py
+++ organizations/schema.py
@@ -11,7 +11,24 @@ from services.google_api.google_places import GooglePlacesAPI
 class OrganizationType(DjangoObjectType):
     class Meta:
         model = Organization
-        fields = "__all__"
+        fields = (
+            # main information
+            'id',
+            'name',
+            'place_id',
+            'partner',
+
+            # additional details
+            'address',
+            'business_status',
+            'icon',
+            'lat',
+            'lng',
+            'phone_number',
+            'types',
+            'url',
+            'website'
+        )
 
 
 ################################################
@@ -37,113 +54,5 @@ class Query(graphene.ObjectType):
 
 
 ################################################
-### Mutation
+### Mutations
 ################################################
-class CreateOrganization(graphene.Mutation):
-    organization = graphene.Field(OrganizationType)
-
-    class Arguments:
-        place_id = graphene.String()
-        business_status = graphene.String()
-        icon = graphene.String()
-        name = graphene.String()
-        lat = graphene.Float()
-        lng = graphene.Float()
-        address = graphene.String()
-        phone_number = graphene.String()
-        url = graphene.String()
-        website = graphene.String()
-        types = graphene.List(graphene.String)
-        partner = graphene.Boolean()
-
-    def mutate(
-        self,
-        info,
-        place_id=None,
-        business_status=None,
-        icon=None,
-        name=None,
-        lat=None,
-        lng=None,
-        address=None,
-        phone_number=None,
-        url=None,
-        website=None,
-        types=None,
-        partner=None,
-    ):
-
-        try:
-            organization = Organization.objects.get(place_id=place_id)
-        except:
-            organization = None
-
-        if organization is None:
-            organization = Organization(
-                place_id=place_id,
-                business_status=business_status,
-                icon=icon,
-                name=name,
-                lat=lat,
-                lng=lng,
-                address=address,
-                phone_number=phone_number,
-                url=url,
-                website=website,
-                types=types,
-                partner=partner,
-            )
-
-            organization.save()
-            return CreateOrganization(organization=organization)
-        raise Exception('Organization already exists')
-
-
-class OrganizationSearch(graphene.Mutation):
-    organization = graphene.Field(OrganizationType)
-
-    class Arguments:
-        place = graphene.String()
-        location = graphene.String()
-
-    def mutate(self, info, place, location):
-        api = GooglePlacesAPI()
-        data = api.details(place, location)
-        errors = data.get("errors", None)
-
-        if errors:
-            raise errors
-
-        results = data.get("result")
-        place_id = data.get('place_id', "")
-        place_name = results.get("name", "")
-        location = results["geometry"]["location"]
-        lat = location.get("lat", None)
-        lng = location.get("lng", None)
-        business_status = results.get("business_status", "")
-        icon = results.get("icon", "")
-        address = results.get("formatted_address", "")
-        place_phone_number = results.get("formatted_phone_number", "")
-        url = results.get("url", "")
-        types = results.get("types", [])
-        website = results.get("website", "")
-
-        organization = Organization(
-            place_id=place_id,
-            business_status=business_status,
-            icon=icon,
-            name=place_name,
-            lat=lat,
-            lng=lng,
-            address=address,
-            phone_number=place_phone_number,
-            url=url,
-            website=website,
-            types=types,
-        )
-        return OrganizationSearch(organization=organization)
-
-
-class Mutation(graphene.ObjectType):
-    create_organization = CreateOrganization.Field()
-    organization_search = OrganizationSearch.Field()
