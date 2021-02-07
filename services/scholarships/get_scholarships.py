4fb01189caa46ff9042e53ca56505e152ae354f2
Sinthuja Nagalingam <sinthu.n@gmail.com>
2021-02-07 10:54:55 -0800

    added scholarships to database

--- services/scholarships/get_scholarships.py
+++ services/scholarships/get_scholarships.py
@@ -1,16 +1,24 @@
-import requests
-from bs4 import BeautifulSoup
 import csv
 import json
-import re
-from io import StringIO
+import os
+import requests
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+SCRIPT_DIR = os.path.dirname(__file__)
 
+################################################################################
+# This function extracts the data from a scholarship page
+################################################################################
 def extract_data(url):
     response = requests.get(url, headers=HEADERS)
     page = BeautifulSoup(response.text, 'html.parser')
-    # use selineum to click link 
+    # use selineum to click link
     name = page.find('h1').text
     award_info = page.find_all("div", {"class": "award-info-row"})[0].contents
     ammont = None
@@ -30,8 +38,9 @@ def extract_data(url):
 
     description = page.find('li', { "class": "scholdescrip" }).text.replace("ADVERTISEMENT", "").strip('\r\n')
     body = page.find('ul', { "id": "ulScholDetails" })
-    start_contact = False 
+    start_contact = False
     count = 1
+    contact_name = None
     contact_address = None
     contact_city = None
     contact_state = None
@@ -42,7 +51,7 @@ def extract_data(url):
 
     for each in body:
         if each != '\n':
-            if each.text.strip() == "Provider":
+            if each.text.strip() == "Contact":
                 start_contact = True
                 continue
 
@@ -75,77 +84,36 @@ def extract_data(url):
                         contact_ext = info
                     elif "fax:" in info:
                         continue
-                    else: 
+                    else:
                         contact_phone = info
 
     data = {
-        "Name": name, 
-        "Amount": amount, 
-        "Deadline": deadline, 
-        "Available": available, 
-        "Description": description, 
-        "Provider": contact_name, 
-        "Address": contact_address, 
-        "City": contact_city, 
-        "State": contact_state, 
-        "Zipcode": contact_zipcode, 
-        "Email": contact_email, 
-        "Phone": contact_phone, 
-        "Ext": contact_ext, 
+        "Name": name,
+        "Amount": amount,
+        "Deadline": deadline,
+        "Available": available,
+        "Description": description,
+        "Provider": contact_name,
+        "Address": contact_address,
+        "City": contact_city,
+        "State": contact_state,
+        "Zipcode": contact_zipcode,
+        "Email": contact_email,
+        "Phone": contact_phone,
+        "Ext": contact_ext,
     }
 
     return data
 
-def get_scholarship_table(url_list):
-    entire_list = {}
-    count = 1
-    month = 0
-    for name, url in url_list.items():
-        month += 1
-        # sort by earliest deadline
-        if month >= 5:
-            response = requests.get(url + "?sortOrder=duedate&sortDirection=asc", headers=HEADERS)
-            page = BeautifulSoup(response.text, 'html.parser')
-            table = page.find_all(
-                "table", {"class": "scholarshiplistdirectory"})
 
-            # create csv file for tables
-            # with open(f"{name}.csv", "wt") as f:
-            #     f.write(table[0].text)
-
-            urls = table[0].find_all("a")
-            scholarship_urls = {}
-
-            for url in urls[3:]:
-                contact = url.text
-                href = url.get("href")
-                root = "https://www.scholarships.com"
-                scholarship_urls[contact] = root + href
-
-                # extract data from url 
-                # data = extract_data(root + href)
-                # create json file for each scholarship
-                # with open(f'scholarship_data.json', '+a') as f:
-                #     d = json.dumps(data, indent=2, ensure_ascii=False)
-                #     f.write(d + ',')
-
-                print(f' COUNT ===> : {count}')
-                count += 1
-
-            entire_list[name] = scholarship_urls
-            # create json file for entire_list
-            # with open(f'entire_list.json', '+a') as f:
-            #     data = json.dumps(entire_list, indent=2, ensure_ascii=False)
-            #     f.write(data)
-
-    return entire_list
-
-def get_scholarship_urls():
+################################################################################
+# This function gets the scholarship pages based on monthly deadlines
+################################################################################
+def get_scholarships_based_on_deadline(month):
     url = f"https://www.scholarships.com/financial-aid/college-scholarships/scholarship-directory/deadline"
     response = requests.get(url, headers=HEADERS)
     page = BeautifulSoup(response.text, 'html.parser')
-    links = page.find_all(
-        "ul", {"id": "ullist"})
+    links = page.find_all("ul", {"id": "ullist"})
     urls = links[0].find_all("a")
     scholarship_urls = {}
 
@@ -155,8 +123,35 @@ def get_scholarship_urls():
         root = "https://www.scholarships.com"
         scholarship_urls[name] = root + href
 
-    return get_scholarship_table(scholarship_urls)
+    entire_list = {}
+    for name, url in scholarship_urls.items():
+        if month in name:
+            response = requests.get(url + "?sortOrder=duedate&sortDirection=asc", headers=HEADERS)
+            page = BeautifulSoup(response.text, 'html.parser')
+            table = page.find_all("table", {"class": "scholarshiplistdirectory"})
+            urls = table[0].find_all("a")
+            scholarship_urls = {}
 
+            for url in urls[3:]:
+                contact = url.text
+                href = url.get("href")
+                root = "https://www.scholarships.com"
+                scholarship_urls[contact] = root + href
+                data = extract_data(root + href)
+
+                with open(os.path.join(SCRIPT_DIR, 'scholarship_data.json'), '+a') as f:
+                    d = json.dumps(data, indent=2, ensure_ascii=False)
+                    f.write(d + ',')
+
+            entire_list[name] = scholarship_urls
+            with open(os.path.join(SCRIPT_DIR, 'entire_list.json'), '+a') as f:
+                data = json.dumps(entire_list, indent=2, ensure_ascii=False)
+                f.write(data)
+
+
+################################################################################
+# Convert scholarship json file to csv file
+################################################################################
 def make_csvs(json_file):
     json_data = json.load(open(f'{json_file}.json'))
     count = 1
@@ -194,7 +189,7 @@ def make_csvs(json_file):
                 "Website"]
             csv_writer = csv.writer(f)
             csv_writer.writerow(headers)
-            
+
         for data in json_data:
             if month in data["Deadline"]:
 
@@ -232,10 +227,80 @@ def make_csvs(json_file):
                     website
                     )
 
-                # with open(f'{month}.csv', '+a') as f:
-                #     csv_writer = csv.writer(f)
-                #     csv_writer.writerow(each_line)
-                # print(f' COUNT ===> : {count}')
-                count += 1
+                with open(f'{month}.csv', '+a') as f:
+                    csv_writer = csv.writer(f)
+                    csv_writer.writerow(each_line)
 
                 return print(f'{json_file}.json converted to csv file.')
+
+
+################################################################################
+# This function gets the apply links
+################################################################################
+FOLDER_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+# json file of all scholarships from deadline table
+#scholarships = json.load(open(f"{FOLDER_PATH}/csvs/entire_url_list.json"))
+# json file of all deails from scholarships
+#data = json.load(open(f"{FOLDER_PATH}/csvs/scholarship_data.json"))
+
+def get_apply_link(scholarships, data):
+    count = 0
+    options = Options()
+
+    # invisibility option for browser
+    # options.add_argument("--headless")
+
+    chromedriver = "/Users/pd/Desktop/Projects/scrapper/tilt_scripts/common_ds/chromedriver"
+    driver = webdriver.Chrome(executable_path=chromedriver, options=options)
+
+    # resize browser window (width, height) min_width=1000 or login will not be visible
+    driver.set_window_size(1000, 100)
+    driver.set_window_position(0, 0)
+
+    for month in scholarships.keys():
+        for url in scholarships[month].values():
+
+            # find login and click on first open
+            if count == 0:
+                driver.get(f"https://www.scholarships.com/")
+                login = driver.find_element_by_link_text("Login")
+                login.click()
+
+                # input email and password and submit
+                email = driver.find_element_by_css_selector("input[type=email")
+                email.send_keys("tiltaccess@gmail.com")
+                password = driver.find_element_by_css_selector("input[type=password")
+                password.send_keys("tilt1234")
+                password.send_keys(Keys.RETURN)
+
+            # go to scholarship url
+            try:
+                driver.get(url)
+                apply = driver.find_element_by_id("apply-now-button")
+                href = apply.get_attribute("href")
+                start = href.index("http")
+                end = href.index("',")
+                scholarship_url = href[start:end]
+
+                # add scholarship_url to data
+                data[count]["scholarship_url"] = scholarship_url
+
+                # with open(f'new_data.json', '+a') as f:
+                #     d = json.dumps(data[count], indent=2, ensure_ascii=False)
+                #     f.write(d + ',')
+
+                # print(f" ======> COUNT {count} : {scholarship_url}")
+
+            except:
+                pass
+
+            count += 1
+
+    # closer browser when done
+    return driver.close()
+
+
+################################################################################
+# Run these functions
+################################################################################
+get_scholarships_based_on_deadline(month="March")
