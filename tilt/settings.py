697e18a1daca1d50e3ace0bfa60deb0424f19f15
Sinthuja Nagalingam <sinthu.n@gmail.com>
2021-04-04 18:43:15 -0700

    add all secure settings except ssl

--- tilt/settings.py
+++ tilt/settings.py
@@ -332,17 +332,17 @@ elif ENVIRONMENT == 'production':
         '.tiltstaging.com',
         '.tiltstaging.dev',
     ]
-    #CSRF_COOKIE_SECURE = True
-    #SECURE_BROWSER_XSS_FILTER = True
-    #SECURE_CONTENT_TYPE_NOSNIFF = True
-    #SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    #SECURE_HSTS_SECONDS = 3600
-    #SECURE_HSTS_PRELOAD = True
-    #SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    #SECURE_REFERRER_POLICY = 'same-origin'
-    #SECURE_SSL_REDIRECT = True
-    #SESSION_COOKIE_SECURE = True
-    #X_FRAME_OPTIONS = 'DENY'
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_PRELOAD = True
+    # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_REFERRER_POLICY = 'same-origin'
+    # SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    X_FRAME_OPTIONS = 'DENY'
 
 
 ################################################################################
